a0f5ca7b3d7b77ae6153397740ebee311c09e9f4
panahian18 <pajan@MacBook-Pro.local>
2025-09-09 16:14:00 -0400

    added ch. 5 ex 1

---
+++ chapter5-exercises.js
@@ -0,0 +1,11 @@
+/*
+Ex.1
+Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array that has all the elements of the 
+original arrays.
+
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+
+const flattenedArray = arrays.reduce((flat, current) => flat.concat(current));
