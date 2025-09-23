0d6937f73f30f086f06d4083c8fec2d4a8ace5f9
panahian18 <pajan.pl@gmail.com>
2025-09-23 18:17:00 -0400

    completed ex 5.3

--- chapter5-exercises.js
+++ chapter5-exercises.js
@@ -32,3 +32,31 @@ loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
+
+/*
+Arrays also have an every method analogous to the some method. This method returns true when the given function returns true for every 
+element in the array. In a way, some is a version of the || operator that acts on arrays, and every is like the && operator.
+
+Implement every as a function that takes an array and a predicate function as parameters. Write two versions, one using a 
+loop and one using the some method.
+*/
+
+function every(array, test) {
+  for (let el of array) {
+    if (!test(el)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function every2(array, test) {
+  return !array.some(el => !test(test));
+}
+
+console.log(every([1, 3, 5], n => n < 10));
+// → true
+console.log(every([2, 4, 16], n => n < 10));
+// → false
+console.log(every([], n => n < 10));
+// → true
